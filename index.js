38933f7acf75f9f54651ccb510ca7d4da544a515
Jose Ganchozo <joseganchozo@outlook.com>
2020-06-05 18:01:20 -0500

    add data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const { mongoose }  = require('./database');
 
@@ -9,6 +10,7 @@ app.set('port', process.env.PORT || 3000)
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 //Routes
 app.use('/api/movies', require('./routes/movies.routes'));
